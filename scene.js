7d4876584a58c4511fb38e118bcd0d3066922758
Pablo Fernández Franco <poltero@ovi.com>
2014-09-11 15:37:18 +0200

    Comenzando a crear el prototipo Scene

---
+++ scene.js
@@ -0,0 +1,16 @@
+(function(){
+
+    function Scene(mapFile, player) {
+        this.mapFile = mapFile;
+        this.plataforms = new Array();
+        this.player = player;
+    };
+
+    Scene.prototype = {
+        init: function() {
+            
+        }
+    };
+
+
+})();
