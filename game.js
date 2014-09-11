795a37b6783204579237d3c0ba0d1f3e497fd30b
Pablo Fernández Franco <poltero@ovi.com>
2014-09-11 23:35:58 +0200

    Encapsulado el proceso de inicializar la escena

--- game.js
+++ game.js
@@ -38,46 +38,15 @@ init();
 
 function render() {
     
-    //player.renderRect(ctx);
     currentLevel.render(ctx);
 
-    //console.log(lastTime);
 }
 
 function init() {
 
     currentLevel = new Scene("level1.txt");
 
-    $.get(currentLevel.mapFile, function(data) {
-        var map = data;
-        var posx = 0, posy = 0;
+    currentLevel.init();
 
-        var size = map.length;
-        for(i = 0; i < size; i++) {
-            var c = map.charAt(i);
-            
-            if(c == '-') {
-                currentLevel.plataforms.push(new Entity(posx,posy,null));
-                posx = posx + currentLevel.sizeTile;
-            }
-            else if(c == ' ') {
-                posx = posx + currentLevel.sizeTile;
-            }
-            else if(c == 'p') {
-                //console.log(" LOAD player");
-                currentLevel.player = new Entity(posx, posy,null);
-                //console.log(this.player);
-                posx = posx + currentLevel.sizeTile;
-            }
-            else if(c == '\n') {
-                posx = 0;
-                posy = posy + currentLevel.sizeTile;
-            }
-
-            
-        }
-
-        main();
-
-    });
+    main();
 }
--- scene.js
+++ scene.js
@@ -18,10 +18,47 @@
                 this.plataforms[i].renderRect(ctx);
             }
 
-            //console.log(this.player);
             //Render player
             ctx.fillStyle="#FF0000";
             this.player.renderRect(ctx);
+        },
+
+        init: function() {
+
+            var loadMap = function(data) {
+                var map = data;
+                var posx = 0, posy = 0;
+
+                var size = map.length;
+                for(i = 0; i < size; i++) {
+                    var c = map.charAt(i);
+                    
+                    if(c == '-') {
+                        this.plataforms.push(new Entity(posx,posy,null));
+                        posx = posx + this.sizeTile;
+                    }
+                    else if(c == ' ') {
+                        posx = posx + this.sizeTile;
+                    }
+                    else if(c == 'p') {
+                        //console.log("LOAD player");
+                        this.player = new Entity(posx, posy,null);
+                        //console.log(this.player);
+                        posx = posx + this.sizeTile;
+                    }
+                    else if(c == '\n') {
+                        posx = 0;
+                        posy = posy + this.sizeTile;
+                    }
+                }
+            }
+
+            $.ajax({
+                async: false,
+                type: 'GET',
+                url: this.mapFile,
+                success: $.proxy(loadMap,this)
+            });
         }
     };
 
